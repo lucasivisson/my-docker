05b16f04331cf91c5a0bc2309b809ca1a7857208
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-10 19:40:38 -0300

    🚧 working with proxy reverse

--- 5-challenges/nodejs-proxy-reverse/node/index.js
+++ 5-challenges/nodejs-proxy-reverse/node/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "app-proxy-reverse"
+})
+
 const PORT = 3000;
 
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Running Node.js')
+  connection.connect(function(err) {
+    if (err) throw err;
+    connection.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+      response.send(result);
+    });
+  })
+});
+
+app.post('/create-user', (request, response) => {
+  const sql = `INSERT INTO users(name) values ('Lucas')`
+  connection.query(sql)
+  connection.end()
 });
 
 app.listen(PORT, () => {
