15e779cbc1ad43ca3235dbf40e01e4261f11034a
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-11 10:35:26 -0300

    ✨ finishing challenge

--- 5-challenges/nodejs-proxy-reverse/node/index.js
+++ 5-challenges/nodejs-proxy-reverse/node/index.js
@@ -2,30 +2,36 @@ import express from 'express';
 import mysql from 'mysql';
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "db",
   user: "root",
   password: "root",
-  database: "app-proxy-reverse"
+  database: "app-proxy-reverse",
+  port: 3306,
 })
 
 const PORT = 3000;
 
 const app = express();
 
+const sql = 'CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);'
+connection.connect(function(err) {
+  if (err) throw err
+});
+connection.query(sql);
+
 app.get('/', (request, response) => {
-  connection.connect(function(err) {
-    if (err) throw err;
     connection.query("SELECT * FROM users", function (err, result, fields) {
       if (err) throw err;
       response.send(result);
     });
-  })
 });
 
-app.post('/create-user', (request, response) => {
+app.get('/create-user', (request, response) => {
   const sql = `INSERT INTO users(name) values ('Lucas')`
-  connection.query(sql)
-  connection.end()
+  connection.query(sql, function(err, result, fields) {
+    if (err) throw err;
+    response.send(result);
+  });
 });
 
 app.listen(PORT, () => {
